c31fd7f953a5f4f6c3675f93f5925ef34c406166
courtneyod <courtney.od@gmail.com>
2015-12-01 20:40:49 -0800

    checkpoint 22 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,9 @@
 var animatePoints = function(){
  
+	//return an array with the elements that have the point class
    var points = document.getElementsByClassName('point');
- 
+ console.log("hi i'm working");
+	//revealFirstPoint is the name of the function
    var revealFirstPoint = function() {
      points[0].style.opacity = 1;
      points[0].style.transform = "scaleX(1) translateY(0)";

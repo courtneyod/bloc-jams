da21f49f36495bdb25689265d996ea20ffb5e34b
courtneyod <courtney.od@gmail.com>
2015-12-03 20:15:05 -0800

    second try

--- scripts/landing.js
+++ scripts/landing.js
@@ -15,7 +15,11 @@ for(var i=0; i<points.length; i++){
 }
              };
 
-window.onload = function(){
+window.onload = function
+if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
 	var sellingPoints = document.getElementsByClassName('selling-points')[0];
 	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
  window.addEventListener('scroll', function(event) {

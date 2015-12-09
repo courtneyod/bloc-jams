4d5884257e341ca78b2acabb8d2fd81404f8f21a
courtneyod <courtney.od@gmail.com>
2015-12-08 22:44:19 -0800

    change to utilities.js and landing.js

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,5 +1,10 @@
-function forEach(callback, array){
+//forEach( array, functionBeingCalled )
+function forEach(array, callback){
 	for(var i =0; i< array.length; i++){
-	callback(array[i]);
+		//callback is a function that gets called from something happening
+		callback(array[i]);
 	}
 }
+
+//usually you would use:
+//array.forEach(function(arrayElement){  // do a thing })
